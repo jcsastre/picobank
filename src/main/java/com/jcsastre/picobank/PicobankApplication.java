23c74d49665b63f852916261e2f26ababc2db5a4
Juan Carlos Sastre Iglesias <juan.carlos.sastre.iglesias@gmail.com>
2017-12-15 16:06:53 +0100

    Spring Boot bootstrap

---
+++ src/main/java/com/jcsastre/picobank/PicobankApplication.java
@@ -0,0 +1,12 @@
+package com.jcsastre.picobank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicobankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicobankApplication.class, args);
+	}
+}
